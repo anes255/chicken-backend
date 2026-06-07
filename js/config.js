4f2d631f112ffa75f1c23e6d6ece05059512fc0d
anes255 <anesgaher3000@gmail.com>
2026-06-07 21:30:16 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// Point this at your backend. When the backend serves the frontend,
-// leaving it as '' (same origin) works automatically.
-window.API_BASE = window.location.port === '4000' ? '' : 'http://localhost:4000';
